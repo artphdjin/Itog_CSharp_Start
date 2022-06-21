2691c58a366ce149b4d768b711e48920ea6c88e2
Artem <art.ph.djin@gmail.com>
2022-06-21 09:20:49 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Itog_CSharp_Start
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
